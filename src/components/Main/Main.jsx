0b178b726783118e2a93d2505a9c9963af0e3e56
Pugga <aezakmi2508@gmail.com>
2022-11-03 00:21:23 +0200

    Second Load

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,16 @@
+
+import './main.css';
+
+function Main() {
+  return (
+    <div className="main">
+      <img src="./images/21 - .png" alt="" className='smokeImgLeft' />
+      <img src="./images/04 - .png" alt="" className='smokeImgRight' />
+    <div className='wrapper'>
+        <p>высокие технологии спецэффектов</p>
+    </div>
+    </div>
+  );
+}
+
+export default Main;
