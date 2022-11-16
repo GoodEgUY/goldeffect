2ebe3e04599827ef0c8fcc2f2edf8fe1f3bacf02
Pugga <aezakmi2508@gmail.com>
2022-11-17 01:08:38 +0200

    beta

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -7,7 +7,18 @@ function Main() {
       <img src="./images/21 - .png" alt="" className='smokeImgLeft' />
       <img src="./images/04 - .png" alt="" className='smokeImgRight' />
     <div className='wrapper'>
-        <p>высокие технологии спецэффектов</p>
+      <div className='button'>
+        <p>GITHUB</p>
+      </div>
+      <div className='button'>
+        <p>Linkedin</p>
+      </div>
+      <div className='button'>
+        <p>TG</p>
+      </div>
+      <div className='button'>
+        <p>Inst</p>
+      </div>
     </div>
     </div>
   );
