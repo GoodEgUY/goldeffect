098bf99c8642a6ad93a386eda7c216fba4a81969
Pugga <aezakmi2508@gmail.com>
2022-11-22 11:58:13 +0200

    New Design

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,26 +1,97 @@
-
-import './main.css';
+import "./main.css";
+import { useState } from "react";
 
 function Main() {
+  const [startOpened, setStartOpened] = useState(true);
+
+  const [UxProject, setUxProject] = useState(false);
+  const openStart = () => {
+    setStartOpened(true);
+    setUxProject(false);
+  };
+  const openUxProject = () => {
+    setStartOpened(false);
+    setUxProject(true);
+  };
   return (
-    <div className="main">
-      <img src="./images/21 - .png" alt="" className='smokeImgLeft' />
-      <img src="./images/04 - .png" alt="" className='smokeImgRight' />
-    <div className='wrapper'>
-      <div className='button'>
-        <p>GITHUB</p>
+    <>
+      <div className="main">
+        <div className="wrapper">
+          <div className="portNavigation">
+            <p
+              className={
+                startOpened
+                  ? "aboutPortfolioNavLink activeAbNL"
+                  : "aboutPortfolioNavLink"
+              }
+              onClick={openStart}
+            >
+              Develop
+            </p>
+            <p
+              className={
+                UxProject
+                  ? "aboutPortfolioNavLink activeAbNL"
+                  : "aboutPortfolioNavLink"
+              }
+              onClick={openUxProject}
+            >
+              Other
+            </p>
+          </div>
+          {startOpened ? (
+            <div className="dashbord">
+              <button
+                className="button"
+                onClick={(window.location.href = "https://github.com/GoodEgUY")}
+              >
+                <img src="./images/icon1.png" alt="" className="ico" />
+              </button>
+              <button
+                className="button"
+                onClick={(window.location.href = "https://github.com/GoodEgUY")}
+              >
+                <p>LinkedIN</p>
+                <img src="./images/icon1.png" alt="" className="ico" />
+              </button>
+              <button
+                className="button"
+                onClick={(window.location.href = "https://github.com/GoodEgUY")}
+              >
+                <p>Telegram</p>
+                <img src="./images/Icon5.png" alt="" className="ico" />
+              </button>
+              <button
+                className="button"
+                onClick={(window.location.href = "https://github.com/GoodEgUY")}
+              >
+                <p>Inst</p>
+                <img src="./images/icon1.png" alt="" className="ico" />
+              </button>
+            </div>
+          ) : (
+            <div className="dashbord">
+              <div className="button">
+                <p>GITHUB23</p>
+                <img src="./images/icon1.png" alt="" className="ico" />
+              </div>
+              <div className="button">
+                <p>Linkedin</p>
+                <img src="./images/icon1.png" alt="" className="ico" />
+              </div>
+              <div className="button">
+                <p>TG</p>
+                <img src="./images/Icon5.png" alt="" className="ico" />
+              </div>
+              <div className="button">
+                <p>Inst</p>
+                <img src="./images/icon1.png" alt="" className="ico" />
+              </div>
+            </div>
+          )}
+        </div>
       </div>
-      <div className='button'>
-        <p>Linkedin</p>
-      </div>
-      <div className='button'>
-        <p>TG</p>
-      </div>
-      <div className='button'>
-        <p>Inst</p>
-      </div>
-    </div>
-    </div>
+    </>
   );
 }
 
