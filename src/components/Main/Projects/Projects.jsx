84aba4c0465f0d85775ae9e05d25066f55ac3f42
Pugga <38046181+GoodEgUY@users.noreply.github.com>
2023-06-30 18:31:45 +0300

    Update Projects.jsx

--- src/components/Main/Projects/Projects.jsx
+++ src/components/Main/Projects/Projects.jsx
@@ -5,11 +5,10 @@ const Projects = () => {
   return (
     
     <div className="projects">
-      <a href="https://pugga-champions-dev-agency.netlify.app/">
         <div className="projectItem">
           <img src="./images/champ.png" alt="" className="projectImage" />
           <div className="projectText">
-            <p className="projectName">Champions</p>
+            <p className="projectName">Coming Soon...</p>
             <p className="projectTitle">Marketing Agency</p>
           </div>
         </div>
