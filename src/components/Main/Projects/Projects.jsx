a782c096b53cfd058725ce67d4f0d0081bf40d6c
GoodEgUY <aezakmi2508@gmamil.com>
2024-01-22 21:14:26 +0200

    gg

--- src/components/Main/Projects/Projects.jsx
+++ src/components/Main/Projects/Projects.jsx
@@ -3,19 +3,45 @@ import "./projects.css";
 
 const Projects = () => {
   return (
-    
     <div className="projects">
-      
+      <a href="https://pods.com.ua">
         <div className="projectItem">
-          <img src="./images/champ.png" alt="" className="projectImage" />
+          <img src="./images/pods.png" alt="" className="projectImage" />
+
           <div className="projectText">
-            <p className="projectName">Coming Soon...</p>
-            <p className="projectTitle">Marketing Agency</p>
+            <p className="projectName">PODS</p>
+            <p className="projectTitle">Vape Shop</p>
           </div>
         </div>
-      
+      </a>
+      <a href="https://vipkalyan.com">
+        <div className="projectItem">
+          <img src="./images/vip.png" alt="" className="projectImage" />
+          <div className="projectText">
+            <p className="projectName">VIPKALYAN</p>
+            <p className="projectTitle">Hookah Shop</p>
+          </div>
+        </div>
+      </a>
+      <div className="projectItem">
+        <img src="./images/champ.png" alt="" className="projectImage" />
+        <div className="projectText">
+          <p className="projectName">Coming Soon...</p>
+          <p className="projectTitle">Marketing Agency</p>
+        </div>
+      </div>
+      <div className="projectItem">
+        <img
+          src="./images/touch/homescreen196.png"
+          alt=""
+          className="projectImage"
+        />
+        <div className="projectText">
+          <p className="projectName">Coming Soon...</p>
+          <p className="projectTitle">Business Ecosystem</p>
+        </div>
+      </div>
     </div>
-    
   );
 };
 
